ff307c0066ad4d6feda06d1f5ade859780e8961e
Tobi <38384674+adelleye@users.noreply.github.com>
2022-04-03 15:30:21 -0400

    Map numbers to Die component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./components/Die";
 
 const App = () => {
   //Generate Random numbers between 1 & 6
   const allNewDice = () => {
     const randomNumbers = [];
-
-    for (let i = 0; i < 9; i++) {
-      randomNumbers[i] = Math.floor(Math.random() * 6) + 1;
-      //randomNumbers.push(Math.floor(Math.random() * 6) + 1)
+    for (let i = 0; i < 10; i++) {
+      randomNumbers.push(Math.floor(Math.random() * 6) + 1);
     }
     return randomNumbers;
   };
-  console.log(allNewDice());
+
+  //States
+  const [die, setDie] = useState(allNewDice);
+
+  //Map random numbers to Die component
+  const diceNumbers = die.map((numbers) => <Die value={numbers} />);
 
   return (
     <main>
-      <div className="container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
-      </div>
+      <div className="container">{diceNumbers}</div>
     </main>
   );
 };
