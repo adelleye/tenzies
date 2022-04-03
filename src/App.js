17b11b6dc580b109a849ceb5c0eaaf8c22e07bf4
Tobi <38384674+adelleye@users.noreply.github.com>
2022-04-03 14:22:56 -0400

    Added & styled "Die" component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Die from "./components/Die";
 
 const App = () => {
   return (
     <main>
-      <p>Hello World</p>
+      <div className="container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="10" />
+      </div>
     </main>
   );
 };
---
+++ src/components/Die.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Die = (props) => {
+  return (
+    <div className="die">
+      <p>{props.value}</p>
+    </div>
+  );
+};
+
+export default Die;
